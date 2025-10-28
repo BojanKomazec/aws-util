22d1d1b6c12a4dcbe37a68957f1a286e5145faac
Bojan Komazec <1876278+BojanKomazec@users.noreply.github.com>
2025-10-28 17:03:52 +0000

    feat: add functions which list all lambdas, set and reset lambda concurrency limit

--- util.sh
+++ util.sh
@@ -109,6 +109,81 @@ list_resource_using_security_group() {
     done
 }
 
+list_all_lambda_functions() {
+    local output
+    local exit_code
+    output=$(aws lambda list-functions \
+        --profile "$AWS_PROFILE" \
+        --region "$AWS_REGION" \
+        --query "Functions[].FunctionName" \
+        --output text 2>&1)
+
+    exit_code=$?
+
+    if [ $exit_code -ne 0 ]; then
+        log_error "Failed to list Lambda functions.\nError: $output"
+        return 1
+    fi
+
+    lambda_names=()
+    # Normalize output. Original output contains TAB-delimited lambda names.
+    # We want to have each lambda name in a new line.
+    # Split tab-delimited output into array.
+    readarray -t lambda_names <<< "$(echo "$output" | tr '\t' '\n')"
+
+    log_array_elements true "${lambda_names[@]}"
+}
+
+# If reserved_concurrent_executions is set to -1, it means that the function has unreserved concurrency,
+# meaning it can scale up to account level concurrency limit.
+# If it is set to 0, the function is effectively disabled.
+# Any positive integer value indicates the maximum number of concurrent executions for the function.
+reserve_lambda_concurrency() {
+    local function_name="$1"
+    local reserved_concurrent_executions="$2"
+
+    local output
+    local exit_code
+
+    log_info "Reserving $reserved_concurrent_executions concurrent executions for Lambda function: $function_name"
+
+    output=$(aws lambda put-function-concurrency \
+        --profile "$AWS_PROFILE" \
+        --region "$AWS_REGION" \
+        --function-name "$function_name" \
+        --reserved-concurrent-executions "$reserved_concurrent_executions" 2>&1)
+
+    exit_code=$?
+
+    if [ $exit_code -ne 0 ]; then
+        log_error "Failed to reserve concurrency for Lambda function: $function_name\nError: $output"
+        return 1
+    fi
+
+    log_success "Successfully reserved concurrency for Lambda function: $function_name"
+}
+
+# Removes a concurrent execution limit from a function.
+remove_lambda_concurrency() {
+    local function_name="$1"
+
+    log_info "Removing concurrency limit for Lambda function: $function_name"
+
+    output=$(aws lambda delete-function-concurrency \
+        --profile "$AWS_PROFILE" \
+        --region "$AWS_REGION" \
+        --function-name "$function_name" 2>&1)
+
+    exit_code=$?
+
+    if [ $exit_code -ne 0 ]; then
+        log_error "Failed to remove concurrency limit for Lambda function: $function_name\nError: $output"
+        return 1
+    fi
+
+    log_success "Successfully removed concurrency limit for Lambda function: $function_name"
+}
+
 list_log_group_subscription_filters() {
     local log_group="$1"
     log_info "Log Group: $log_group"
@@ -255,7 +330,8 @@ main() {
 
     # list_resource_using_security_group "$SG_ID"
     # list_lambda_log_group_subscription_filters
-    replace_lambda_log_group_subscription_filters "$OLD_FILTER_DESTINATION_ARN" "$NEW_FILTER_NAME" "$NEW_FILTER_DESTINATION_ARN" "$AWS_CLOUDWATCH_TO_KINESIS_ROLE_ARN"
+    list_all_lambda_functions
+    # replace_lambda_log_group_subscription_filters "$OLD_FILTER_DESTINATION_ARN" "$NEW_FILTER_NAME" "$NEW_FILTER_DESTINATION_ARN" "$AWS_CLOUDWATCH_TO_KINESIS_ROLE_ARN"
 }
 
 main
